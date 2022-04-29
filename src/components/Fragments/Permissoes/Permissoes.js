1ed6cc6f38be16fcb2feba4fca1b2b22d58c2661
mikinglps <micheloblopes@gmail.com>
2022-04-28 22:20:24 -0300

    estilo das permissoes pronto

--- src/components/Fragments/Permissoes/Permissoes.js
+++ src/components/Fragments/Permissoes/Permissoes.js
@@ -6,6 +6,7 @@ const Permissoes = () => {
     const radioRefRead = useRef()
     const radioRefModify = useRef()
     const radioRefDelete = useRef()
+    const [filial, setFilial] = useState('')
     const [aba, setAba] = useState('')
     const [filiais, setFiliais] = useState([])
     const [setores, setSetores] = useState([])
@@ -15,7 +16,15 @@ const Permissoes = () => {
     const [click, setClick] = useState(false)
     const [clickCargo, setClickCargo] = useState(false)
 
-    const handleClickFilial = (id) => {
+    const handleClickFilial = (id, index) => {
+        let doc = document.getElementsByClassName('filial-holder__Permissions');
+        for(let i = 0; i < doc.length; i++){
+            if(i != index){
+                doc[i].style.borderLeft = 'none'
+            }else{
+                doc[index].style.borderLeft = '8px solid rgb(92, 184, 92)'
+            }
+        }
         axios.post('http://localhost:8080/api/v1/setor/filial', {_id: id})
         .then(res => {
             setSetores([...res.data.results])
@@ -23,7 +32,15 @@ const Permissoes = () => {
         })
     }
 
-    const handleClickSetor = (id) => {
+    const handleClickSetor = (id, index) => {
+        let doc = document.getElementsByClassName('setor-holder__Permissions');
+        for(let i = 0; i < doc.length; i++){
+            if(i != index){
+                doc[i].style.borderLeft = 'none'
+            }else{
+                doc[index].style.borderLeft = '8px solid rgb(92, 184, 92)'
+            }
+        }
         axios.post('http://localhost:8080/api/v1/cargos/setor', {_id: id})
         .then(res => {
             setCargos([...res.data])
@@ -95,6 +112,34 @@ const Permissoes = () => {
         }
     }
 
+    const handleClickAbas = (index) => {
+        let doc = document.getElementsByClassName('abas__Permissions');
+        for(let i = 0; i < doc.length; i++){
+            if(i != index){
+                doc[i].style.borderLeft = 'none'
+            }else{
+                doc[index].style.borderLeft = '8px solid rgb(92, 184, 92)'
+            }
+        }
+    }
+
+    const handleClickCargo = (index) => {
+        let doc = document.getElementsByClassName('cargo-holder__Permissions');
+        for(let i = 0; i < doc.length; i++){
+            if(i != index){
+                doc[i].style.borderLeft = 'none'
+            }else{
+                doc[index].style.borderLeft = '8px solid rgb(92, 184, 92)'
+            }
+        }
+    }
+
+    useEffect(() => {
+        setClick(false)
+        setClickCargo(false)
+        setSelectedCargo('')
+    }, [filial])
+
     useEffect(() => {
         radioRefDelete.current.checked = false
         radioRefRead.current.checked = false
@@ -123,8 +168,8 @@ const Permissoes = () => {
             <h2>Selecione uma filial</h2>
             {filiais.map((data, index) => {
                 return(
-                    <section className='holder-single' data-id={index} key={index}>
-                    <div className='filial-holder__Permissions' onClick={() => {handleClickFilial(data._id)}}>
+                    <section className='holder-single' key={index}>
+                    <div className='filial-holder__Permissions' onClick={(e) => {handleClickFilial(data._id, index); setFilial(e.target.innerHTML)}}>
                         <p>{data.titulo}</p>
                     </div>
                     </section>
@@ -135,7 +180,7 @@ const Permissoes = () => {
             {click ? <h2>Selecione um setor</h2> : null}
             {click ? setores.map((value, index) => {
                 return(
-                <div key={index} className='setor-holder__Permissions' onClick={() => {handleClickSetor(value._id)}}>
+                <div key={index} className='setor-holder__Permissions' onClick={() => {handleClickSetor(value._id, index)}}>
                 <p>{value.titulo}</p>
                 </div>
                 )
@@ -145,7 +190,7 @@ const Permissoes = () => {
                 {clickCargo ? <h2>Selecione um cargo</h2> : null}
                 {clickCargo ? cargos.map((value, index) => {
                     return(
-                        <div key={index} className='cargo-holder__Permissions' onClick={() => {setSelectedCargo(value._id)}}>
+                        <div key={index} className='cargo-holder__Permissions' onClick={() => {setSelectedCargo(value._id); handleClickCargo(index)}}>
                             <p>{value.titulo}</p>
                         </div>
                     )
@@ -155,11 +200,11 @@ const Permissoes = () => {
             <div style={selectedCargo ? {display: 'flex'} : {display: 'none'}} className='permissions_selection'>
                 <div className='sidebar__Permissions'>
                 <ul>
-                <li onClick={(e) => {setAba(e.target.innerHTML)}}>Gerenciar</li>
+                <li className='abas__Permissions' onClick={(e) => {setAba(e.target.innerHTML); handleClickAbas(0)}}>Gerenciar</li>
                 {allSetores.map((value, index) => {
                     return(
                         
-                            <li onClick={(e) => {setAba(value._id)}}>{value.titulo}</li>
+                            <li className='abas__Permissions' onClick={(e) => {setAba(value._id); handleClickAbas(index + 1);}}>{value.titulo}</li>
                        
                     )
                 })}
@@ -173,16 +218,16 @@ const Permissoes = () => {
                             <h3>Tipos de permissao</h3>
                             <label>Apenas Leitura</label>
                             <label>Ler e Modificar</label>
-                            <label>Pode deletar</label>
+                            <label>Ler, modificar e deletar</label>
                             </div>
                             <div className='inputs'>
                             <h3>Marque a permissao</h3>
                             <input type='radio' name='ref' ref={radioRefRead}/>
                             <input type='radio' name='ref' ref={radioRefModify}/>
                             <input type='radio' name='ref' ref={radioRefDelete}/>
-                            <button type='button' onClick={() => {save();}}>Save</button>
                             </div>    
                         </div>
+                        <button type='button' onClick={() => {save();}}>Salvar</button>
                     </form>
                 </div>
             </div>
